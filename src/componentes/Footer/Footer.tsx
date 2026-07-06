e8b8afddb4e73f4f97d0041fe2b9d245fdeaa1f6
Thais Souza <84544053+thais-souza311@users.noreply.github.com>
2026-07-06 10:08:29 -0300

    feat: adiciona componente de rodape

---
+++ src/componentes/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import "@/componentes/Footer/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>Acervo Digital — livros usados com nova história</p>
+    </footer>
+  );
+}
+
