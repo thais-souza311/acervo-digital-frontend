023f533b13a6732459c0ad4ea767a30ce0e5987c
Thais Souza <84544053+thais-souza311@users.noreply.github.com>
2026-07-07 17:42:52 -0300

    feat: adiciona pagina de cadastro de livro

---
+++ src/app/livros/criar/page.tsx
@@ -0,0 +1,23 @@
+import LivroForm from "@/componentes/LivroForm/LivroForm";
+import { getMe } from "@/services/auth.services";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function CriarLivroPage() {
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore.toString();
+  const token = cookieStore.get("token");
+
+  if (!token) {
+    redirect("/login");
+  }
+
+  const user = await getMe(cookieHeader);
+
+  if (user.perfil !== "ADMIN") {
+    redirect("/");
+  }
+
+  return <LivroForm />;
+}
+
