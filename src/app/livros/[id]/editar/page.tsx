51fb379517bc92ec43ccb6cfca1be0d750b727a1
Thais Souza <84544053+thais-souza311@users.noreply.github.com>
2026-07-07 17:42:52 -0300

    feat: adiciona pagina de edicao de livro

---
+++ src/app/livros/[id]/editar/page.tsx
@@ -0,0 +1,34 @@
+import LivroForm from "@/componentes/LivroForm/LivroForm";
+import { getMe } from "@/services/auth.services";
+import { getLivro } from "@/services/livro.services";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface Props {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function EditarLivroPage({ params }: Props) {
+  const { id } = await params;
+
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore.toString();
+  const token = cookieStore.get("token");
+
+  if (!token) {
+    redirect("/login");
+  }
+
+  const user = await getMe(cookieHeader);
+
+  if (user.perfil !== "ADMIN") {
+    redirect("/");
+  }
+
+  const livro = await getLivro(id, cookieHeader);
+
+  return <LivroForm livro={livro} />;
+}
+
